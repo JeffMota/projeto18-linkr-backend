2d894d4e8fb9e6643b23c2f5759167a6f2eece4d
JeffMota <jmota906@gmail.com>
2023-03-07 15:57:38 -0300

    refactor: repositories with clas

--- src/controller/Auth.controller.js
+++ src/controller/Auth.controller.js
@@ -1,21 +1,23 @@
 import dotenv from "dotenv"
 import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
-import { emailCheck, userRegistry } from "../repository/auth.repository.js"
+import AuthRepository from "../repository/auth.repository.js"
 dotenv.config()
+
+const authRepository = new AuthRepository()
 
 //Cadastro de usuário
 export async function signup(req, res) {
     const { username, email, password, picture_url } = req.body
     try {
 
-        const alreadyExist = await emailCheck(email)
+        const alreadyExist = await authRepository.emailCheck(email)
 
         if (alreadyExist.rows.length > 0) return res.sendStatus(409)
 
         const hashedPassword = bcrypt.hashSync(password, 10)
 
-        userRegistry(username, email, hashedPassword, picture_url)
+        authRepository.userRegistry(username, email, hashedPassword, picture_url)
 
         res.sendStatus(201)
     } catch (error) {
@@ -29,7 +31,7 @@ export async function signin(req, res) {
     const { email, password } = req.body
 
     try {
-        const user = await emailCheck(email)
+        const user = await authRepository.emailCheck(email)
         if (user.rows.length < 1) return res.sendStatus(401)
         const passwordIsCorrect = bcrypt.compareSync(password, user.rows[0].password)
         if (!passwordIsCorrect) return res.sendStatus(401)
--- src/repository/auth.repository.js
+++ src/repository/auth.repository.js
@@ -1,9 +1,21 @@
 import { db } from "../config/database.connect.js";
 
-export async function emailCheck(email) {
-    return await db.query(`SELECT * FROM users WHERE email = $1;`, [email])
+class AuthRepository {
+
+    async emailCheck(email) {
+        return await db.query(`SELECT * FROM users WHERE email = $1;`, [email])
+    }
+
+    async userRegistry(username, email, hashedPassword, picture_url) {
+        return await db.query(
+            `INSERT INTO users (
+                username, 
+                email, 
+                password, 
+                picture_url
+            ) VALUES ($1, $2, $3, $4);`,
+            [username, email, hashedPassword, picture_url])
+    }
 }
 
-export async function userRegistry(username, email, hashedPassword, picture_url) {
-    await db.query(`INSERT INTO users (username, email, password, picture_url) VALUES ($1, $2, $3, $4);`, [username, email, hashedPassword, picture_url])
-}
+export default AuthRepository
