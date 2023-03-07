5a3d7e337d46d6eb1167640e2d8471bbca7a22ad
JeffMota <jmota906@gmail.com>
2023-03-07 11:53:05 -0300

    chore: dumpDB

---
+++ dump.sql
@@ -0,0 +1,88 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 14.6 (Ubuntu 14.6-0ubuntu0.22.04.1)
+-- Dumped by pg_dump version 14.6 (Ubuntu 14.6-0ubuntu0.22.04.1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.users (
+    id integer NOT NULL,
+    email text NOT NULL,
+    password text NOT NULL,
+    username character varying(50) NOT NULL,
+    picture_url text NOT NULL
+);
+
+
+--
+-- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.users_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
+
+
+--
+-- Name: users id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
+
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+INSERT INTO public.users VALUES (1, 'jeff@email.com', '$2b$10$5dX.VIm7Lw.AAcP4EUERA.CsBIJbgcRnbx/2Kb9r6XdK1TON.5X2S', 'Jefferson', 'https://uploads.jovemnerd.com.br/wp-content/uploads/2023/03/anya__y3q1ops-1210x544.jpg');
+
+
+--
+-- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.users_id_seq', 1, true);
+
+
+--
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
--- src/controller/Auth.controller.js
+++ src/controller/Auth.controller.js
@@ -1,7 +1,7 @@
-import { db } from "../config/database.connect.js"
 import dotenv from "dotenv"
 import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
+import { emailCheck, userRegistry } from "../repository/auth.repository.js"
 dotenv.config()
 
 //Cadastro de usuário
@@ -9,13 +9,13 @@ export async function signup(req, res) {
     const { username, email, password, picture_url } = req.body
     try {
 
-        const alreadyExist = await db.query(`SELECT * FROM users WHERE email = $1;`, [email])
+        const alreadyExist = await emailCheck(email)
 
         if (alreadyExist.rows.length > 0) return res.sendStatus(409)
 
         const hashedPassword = bcrypt.hashSync(password, 10)
 
-        await db.query(`INSERT INTO users (username, email, password, picture_url) VALUES ($1, $2, $3, $4);`, [username, email, hashedPassword, picture_url])
+        userRegistry(username, email, hashedPassword, picture_url)
 
         res.sendStatus(201)
     } catch (error) {
@@ -29,7 +29,7 @@ export async function signin(req, res) {
     const { email, password } = req.body
 
     try {
-        const user = await db.query(`SELECT * FROM users WHERE email = '${email}';`)
+        const user = await emailCheck(email)
         if (user.rows.length < 1) return res.sendStatus(401)
         const passwordIsCorrect = bcrypt.compareSync(password, user.rows[0].password)
         if (!passwordIsCorrect) return res.sendStatus(401)
