3a9175a03039687ada12a93958fa1db6481adffd
Mateus <mateuskvasconcelos@gmail.com>
2023-03-08 07:50:25 -0300

    Help to understand the problem in the token validation

--- src/middleware/ValidateToken.js
+++ src/middleware/ValidateToken.js
@@ -1,19 +1,28 @@
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
 export default function validateToken() {
+  return async (req, res, next) => {
+    const { authorization } = req.headers;
+    if (!authorization) return res.sendStatus(401);
+    const token = authorization.replace("Bearer ", "");
+    jwt.verify(token, process.env.SECRET_JWT, (err, decoded) => {
+      if (err) return res.sendStatus(401);
 
-    return async (req, res, next) => {
-        const { authorization } = req.headers
-        if (!authorization) return res.sendStatus(401)
-        const token = authorization.replace('Bearer ', '')
+      res.locals.userId = decoded.userId;
+      next();
+    });
+  };
+}
 
-
-        jwt.verify(token, process.env.SECRET_JWT, (err, decoded) => {
-            if (err) return res.sendStatus(401)
-
-            res.locals.userId = decoded.userId
-
-            next()
-        })
-    }
+export async function tokenValidation(req, res, next) {
+  const { authorization } = req.headers;
+  const secretKey = process.env.SECRET_JWT;
+  const token = authorization?.replace("Bearer ", "");
+  try {
+    const data = jwt.verify(token, secretKey);
+    res.locals.userId = data.userId;
+    next();
+  } catch {
+    return res.status(401).send("Your session expired, sign-in again.");
+  }
 }
