07fc52d038d5a73b5854bf3df6e910c422f180e0
Telmo Jr <108095100+ttelmo1@users.noreply.github.com>
2023-03-10 20:33:58 -0300

    Update Posts.schemas.js

--- src/schemas/Posts.schemas.js
+++ src/schemas/Posts.schemas.js
@@ -3,5 +3,5 @@ import joi from "joi";
 export const postsSchema = joi.object({
   url: joi.string().uri().required(),
   description: joi.string(),
-  urlTitle: joi.string(),
+  urlTitle: joi.string()
 });
