28f4f20a194e092935e632cc6708c8126fadb831
leonardoyusuke <leoyuhirano@gmail.com>
2023-03-25 17:26:51 +0900

    timeline for user

--- src/repository/posts.repository.js
+++ src/repository/posts.repository.js
@@ -52,7 +52,7 @@ class PostsRepository {
       LEFT JOIN users ON posts."userId" = users.id
       LEFT JOIN likes ON posts.id = likes."postId"
       LEFT JOIN follows f ON f."followerId" = $1
-      WHERE  f."followingId" = posts."userId"
+      where  f."followingId" = posts."userId"
       GROUP BY posts.id, posts.id, users.username, users."pictureUrl", posts.url, posts.description, posts."urlTitle", posts."urlDescription", posts."urlImage"
       ORDER BY posts."createdAt" DESC
       LIMIT 20;`,[userId]
@@ -93,10 +93,33 @@ class PostsRepository {
   }
   async postGetUsername(id) {
     return await db.query(
-      `SELECT users.username ,posts.url, posts.description FROM posts 
-    LEFT JOIN users 
-    ON posts."userId" = users.id
-    WHERE users.id = $1`,
+      `SELECT posts.id,posts."userId", users.username, users."pictureUrl", 
+      posts.url, posts.description, posts."urlTitle", 
+      posts."urlDescription", posts."urlImage",COUNT(likes."postId") AS likes,
+   (SELECT CASE 
+        WHEN (SELECT COUNT(*) FROM likes WHERE "postId" = posts.id) = 0 
+        THEN '[]'::json 
+        ELSE json_agg(DISTINCT users.username) 
+      END
+      FROM likes
+      LEFT JOIN users ON likes."userId" = users.id
+      WHERE likes."postId" = posts.id
+      ) AS "likesUsernames",
+      (SELECT CASE 
+        WHEN (SELECT COUNT(*) FROM likes WHERE "postId" = posts.id) = 0 
+        THEN '[]'::json 
+        ELSE json_agg(DISTINCT likes."userId") 
+      END
+      FROM likes
+      WHERE likes."postId" = posts.id
+      ) AS "likesUserId"   
+FROM posts
+LEFT JOIN users ON posts."userId" = users.id
+LEFT JOIN likes ON posts.id = likes."postId"
+where posts."userId"= $1
+GROUP BY posts.id, posts.id, users.username, users."pictureUrl", posts.url, posts.description, posts."urlTitle", posts."urlDescription", posts."urlImage"
+ORDER BY posts."createdAt" DESC
+LIMIT 20;`,
       [id]
     );
   }
