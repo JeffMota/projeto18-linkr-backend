27b5e5b45b9fc7e810dc78d70cc00a24924fb110
leonardoyusuke <leoyuhirano@gmail.com>
2023-03-25 15:59:38 +0900

    front done

--- src/repository/search.repository.js
+++ src/repository/search.repository.js
@@ -3,10 +3,10 @@ import { db } from "../config/database.connect.js";
 export async function search(username,userId) {
         // return  db.query(`SELECT * FROM users WHERE username LIKE '${username}%'
         // LIMIT 5`)
-        return db.query (`SELECT u.*, (f."followerId" IS NOT NULL) AS seguindo
+        return db.query (`SELECT u.*, (f."followerId" IS NOT NULL) AS following
         FROM users u
         LEFT JOIN follows f ON f."followerId" = '${userId}' AND f."followingId" = u.id
         WHERE u.username LIKE '${username}%'
-        ORDER BY seguindo DESC`
+        ORDER BY following DESC`
    )
 }
